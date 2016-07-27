7dd9df69628a447e740bfb61e1fb7c83d20ff040
Pablo Munoz <pfariasmunoz@gmail.com>
2016-07-26 21:31:12 -0300

    Arranging and designing dialogs and forms

--- src/com/pfariasmunoz/swing/gui/PrefsDialog.java
+++ src/com/pfariasmunoz/swing/gui/PrefsDialog.java
@@ -1,15 +1,23 @@
 package com.pfariasmunoz.swing.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.border.Border;
 
 public class PrefsDialog extends JDialog {
     
@@ -19,6 +27,7 @@ public class PrefsDialog extends JDialog {
     private SpinnerNumberModel spinnerModel;
     private JTextField userField;
     private JPasswordField passField;
+    private PrefsListener prefsListener;
 
     public PrefsDialog(JFrame parent) {
         super(parent, "Preferences", false);
@@ -34,71 +43,17 @@ public class PrefsDialog extends JDialog {
         
         passField.setEchoChar('*');
         
-        setLayout(new GridBagLayout());
-        GridBagConstraints gc = new GridBagConstraints();
-        
-        gc.gridy = 0;
-        
-        //=== First Row ===
-        
-        gc.weightx = 1;
-        gc.weighty = 1;
-        gc.fill = GridBagConstraints.NONE;
-        
-        gc.gridx = 0;
-        
-        add(new JLabel("User: "), gc);
-        
-        gc.gridx++;
-        add(userField, gc);
-        
-        //=== Next Row ===
-        
-        gc.gridy++;
-        
-        gc.weightx = 1;
-        gc.weighty = 1;
-        gc.fill = GridBagConstraints.NONE;
-        
-        gc.gridx = 0;
-        
-        add(new JLabel("Port: "), gc);
-        
-        gc.gridx++;
-        add(portSpinner, gc);
-        
-         //=== Next Row ===
-        
-        gc.gridy++;
-        
-        gc.weightx = 1;
-        gc.weighty = 1;
-        gc.fill = GridBagConstraints.NONE;
-        
-        gc.gridx = 0;
-        
-        add(new JLabel("Password: "), gc);
-        
-        gc.gridx++;
-        add(passField, gc);
-        
-        //=== Next Row ===
-        
-        gc.gridy++;
-        
-        gc.gridx = 0;
-        add(okButton, gc);
-        
-        gc.gridx++;
-        add(cancelButton, gc);
+        layoutControls();
         
         okButton.addActionListener(l -> {
-            Integer value = (Integer) portSpinner.getValue();
+            Integer port = (Integer) portSpinner.getValue();
             
             String user = userField.getText();
             char[] password = passField.getPassword();
             
-            System.out.println(user + ": " + new String(password));
+            if(prefsListener != null) {
+                prefsListener.preferencesSet(user, new String(password), port);
+            }
             
             setVisible(false);
         });
@@ -108,8 +63,108 @@ public class PrefsDialog extends JDialog {
             setVisible(false);
         });
         
-        setSize(400, 300);
+        setSize(340, 250);
         setLocationRelativeTo(parent);
+    }
+
+    private void layoutControls() {
+        
+        JPanel controlsPanel = new JPanel();
+        JPanel buttonsPanel = new JPanel();
+        
+        int space = 15;
+        Border spaceBorder = BorderFactory.createEmptyBorder(space, space, space, space);
+        Border titleBorder = BorderFactory.createTitledBorder("Database Preferences");
+        
+        controlsPanel.setBorder(BorderFactory.createCompoundBorder(spaceBorder, titleBorder));
+        
+        controlsPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+        
+        gc.gridy = 0;
+        
+        Insets rightPadding = new Insets(0, 0, 0, 15);
+        Insets noPadding = new Insets(0, 0, 0, 0);
+
+        
+        //=== First Row ===
+        
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+        
+        gc.gridx = 0;
+        gc.anchor = GridBagConstraints.EAST;
+        gc.insets = rightPadding;
+        controlsPanel.add(new JLabel("User: "), gc);
+        
+        gc.gridx++;
+        gc.anchor = GridBagConstraints.WEST;
+        gc.insets = noPadding;
+        controlsPanel.add(userField, gc);
+        
+        //=== Next Row ===
+        
+        gc.gridy++;
+        
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+        
+        gc.gridx = 0;
+        gc.anchor = GridBagConstraints.EAST;
+        gc.insets = rightPadding;
+        controlsPanel.add(new JLabel("Password: "), gc);
+        
+        gc.gridx++;
+        gc.anchor = GridBagConstraints.WEST;
+        gc.insets = noPadding;
+        controlsPanel.add(passField, gc);
+        
+        //=== Next Row ===
+        
+        gc.gridy++;
+        
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+        
+        gc.gridx = 0;
+        gc.anchor = GridBagConstraints.EAST;
+        gc.insets = rightPadding;
+        controlsPanel.add(new JLabel("Port: "), gc);
+        
+        gc.gridx++;
+        gc.anchor = GridBagConstraints.WEST;
+        gc.insets = noPadding;
+        controlsPanel.add(portSpinner, gc);
+        
+        
+        
+        //=== Buttons ===
+        
+        buttonsPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        buttonsPanel.add(okButton);
+        buttonsPanel.add(cancelButton);
+        
+        Dimension buttonSize = cancelButton.getPreferredSize();
+        okButton.setPreferredSize(buttonSize);
+        
+        // Add sub panels to dialog
+        setLayout(new BorderLayout());
+        add(controlsPanel, BorderLayout.CENTER);
+        add(buttonsPanel, BorderLayout.SOUTH);
+    }
+    
+    public void setDefaults(String user, String password, int port) {
+        userField.setText(user);
+        passField.setText(password);
+        portSpinner.setValue(port);
+        
+    }
+
+    void setPrefsListener(PrefsListener prefsListener) {
+        this.prefsListener = prefsListener;
     }
     
         
